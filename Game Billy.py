81a828c05c23aff0d08c882f8eb697cf2cb3bd9f
Богдан Ковбасюк <bkovbasyk@gmail.com>
2021-11-22 20:36:27 +0200

    Create Game Billy.py
    Try to guess number of dicks

---
+++ Game Billy.py
@@ -0,0 +1,30 @@
+import random 
+
+tries = 0
+
+number = random.randint(1, 50)
+
+print("Welcome to the club body, My name is Billy Heringtone.Try to guess How many dick I was sucking yesterday")
+print("So college boy\nHow many dicks Hmmmmmmmmmm\nMy DUNGGEON MASTER")
+while tries < 6:
+    guess = int(input("Enter the number:"))
+
+    tries +=1
+
+    if guess > number:
+        print("You push me right into the uterus")
+
+    if guess < number:
+        print("Deeper my Master")
+
+    if guess == number:
+        print(f"THANKU SIRRRRRRRRRRRRRRRRRRRRRRRRRRRRR\nYou guess\nOyyyyy You guessed How dicks I emptied, Yuy guessed my number in {tries} guesses")
+
+    if guess != number and tries == 6:
+        print(f"O SHIT I'M SORRY\nYou lost\nBilly disappointed\nI suction semen {number} dicks")
+
+
+
+
+
+
